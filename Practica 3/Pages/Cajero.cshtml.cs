e7a30ec20fd624fd6d6093c0f82aed7f289cba21
Jesus Santos <enmanuel1704@gmail.com>
2021-08-30 08:35:31 -0400

    Revert ""
    This reverts commit dbadce43136febe4dda30acfb0dac126d8c76c3e.

--- Practica 3/Pages/Cajero.cshtml.cs
+++ Practica 3/Pages/Cajero.cshtml.cs
@@ -7,10 +7,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Practica_3.Pages
 {
-
+   
     public class CajeroModel : PageModel
     {
-        Dictionary<int, int> billetes;
+        Dictionary<int, int> billetes ;
         public CajeroModel()
         {
             billetes = new Dictionary<int, int>();
@@ -22,7 +22,7 @@ namespace Practica_3.Pages
         {
 
         }
-        public void OnPost(string banco = "", int monto = 0)
+        public void OnPost(string banco ="", int monto=0)
         {
             if (monto % 100 != 0)
             {
@@ -50,71 +50,28 @@ namespace Practica_3.Pages
             }
 
         }
-        private Dictionary<int, int> dispensarBilletes(int monto)
+        private Dictionary<int,int> dispensarBilletes(int monto)
         {
-            Dictionary<int, int> auxbilletes = new Dictionary<int, int>(billetes);
-            
-            var retiro = new Dictionary<int, int>();
-            if (monto >= 1000)
-            {
-                retiro.Add(1000, 0);
-                int cantidad1000 = monto / 1000;
-                if (cantidad1000 <= billetes[1000])
-                {
-                    monto -= cantidad1000 * 1000;
-                    retiro[1000] = cantidad1000;
-                    billetes[1000] -= cantidad1000;
-                }
-                else
-                {
-                    monto -= billetes[1000] * 1000;
-                    retiro[1000] = billetes[1000];
-                    billetes[1000] -= billetes[1000];
-                }
 
+            int cantidad1000 = monto / 1000;
+            if (cantidad1000 <= billetes[1000])
+            {
+                billetes[1000] -= cantidad1000;
+                monto -= cantidad1000 * 1000;
             }
-
-            if (monto >= 500)
+            else
             {
-                retiro.Add(500, 0);
-                int cantidad500 = monto / 500;
-                if (cantidad500 <= billetes[500])
-                {
-                    monto -= cantidad500 * 500;
-                    retiro[500] = cantidad500;
-                    billetes[500] -= cantidad500;
-                }
-                else
-                {
-                    monto -= billetes[500] * 500;
-                    retiro[500] = billetes[500];
-                    billetes[500] -= billetes[500];
-                }
-
-            }
-
-            if (monto >= 100)
-            {
-                retiro.Add(100, 0);
-                int cantidad500 = monto / 100;
-                if (cantidad500 <= billetes[500])
-                {
-                    monto -= cantidad500 * 100;
-                    retiro[100] = cantidad500;
-                    billetes[100] -= cantidad500;
-                }
-                else
-                {
-                    monto -= billetes[100] * 100;
-                    retiro[100] = billetes[100];
-                    billetes[100] -= billetes[100];
-                }
-
+                billetes[1000] -= billetes[1000];
+                monto -= billetes[1000] * 1000;
             }
 
 
+            int cantidad500 = monto / 500;
+            int cantidad100 = monto / 100;
 
-            return retiro;
+
+
+            return
         }
     }
 }
