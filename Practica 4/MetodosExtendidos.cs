5dbd9220e73d8c2249902b56c644a4676b71428f
Jesus Santos <enmanuel1704@gmail.com>
2021-09-17 12:36:59 -0400

    Practica 4

---
+++ Practica 4/MetodosExtendidos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4
+{
+    public static class MetodosExtendidos
+    {
+        public static List<int> Pares(this List<int> lista)
+        //=> lista.FindAll(p => p%2 == 0); //En landa expresion
+        {
+            return lista.FindAll(delegate (int x) { return x % 2 == 0; }); 
+        }
+    }
+}
---
+++ Practica 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
+            foreach (var item in nums.Pares())
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public bool EsImpar(int x)
+        {
+            if (x % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public double CalcularITBIS(double precio)
+        {
+            var itebis = precio * 0.18;
+            return itebis;
+        }
+
+
+        public DateTime GetDate()
+        {
+            var fecha = DateTime.Now;
+            return fecha;
+        }
+    }
+}
